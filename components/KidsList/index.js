ea4569b9dac339d6a9fc2d082da7cca2f28b3754
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-07-31 15:16:42 +0200

    fix output

--- components/KidsList/index.js
+++ components/KidsList/index.js
@@ -1,15 +1,17 @@
 import useSWR from "swr";
 import { StyledHeading, StyledList } from "./KidsList.styled";
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function KidsList() {
-  const { data, isLoading } = useSWR("/api/kids");
+  const { data, isLoading } = useSWR("/api/kids", fetcher);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
   if (!data) {
-    return;
+    return <h1>Where are the kids???</h1>;
   }
 
   return (
@@ -18,6 +20,7 @@ export default function KidsList() {
       <StyledList>
         {data.map((kid) => (
           <li key={kid._id}>
+          {kid.name}
           </li>
         ))}
       </StyledList>
