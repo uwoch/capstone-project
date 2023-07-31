4cbd32ab9b2485ff3899395ca897efd69ca694df
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-07-31 12:44:36 +0200

    add  kidslist component

---
+++ components/KidsList/index.js
@@ -0,0 +1,27 @@
+import useSWR from "swr";
+import { StyledHeading, StyledList } from "./KidsList.styled";
+
+export default function KidsList() {
+  const { data, isLoading } = useSWR("/api/kids");
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  return (
+    <>
+      <StyledHeading>All Kids</StyledHeading>
+      <StyledList>
+        {data.map((kid) => (
+          <li key={kid._id}>
+            <StyledLink href={`/${kid._id}`}>{kid.name}</StyledLink>
+          </li>
+        ))}
+      </StyledList>
+    </>
+  );
+}
