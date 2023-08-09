35cd896fcf854425bdc2f5d74d42e632277ffb8f
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-08-09 15:59:18 +0200

    fix error

--- components/KidsList/KidsList.js
+++ components/KidsList/KidsList.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import useSWR from "swr";
 import { StyledHeading, StyledList, StyledListContainer,
     StyledListItem, StyledImage, StyledParagraph, StyledLink } from "./KidsList.styled";
