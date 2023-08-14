a2bf876c00f480c3684621f722f06b02fbcf14f4
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-08-14 19:25:42 +0200

    change api

--- pages/api/kids/[id].js
+++ pages/api/kids/[id].js
@@ -3,9 +3,10 @@ import Kid from "@/db/models/Kid";
 
 export default async function handler(request, response) {
   await dbConnect();
+  const { id } = request.query;
 
   if (request.method === "GET") {
-    const kid = await Kid.findById(request.query.id);
+    const kid = await Kid.findById(id).populate("events");
 
     if (!kid) {
       return response.status(404).json({ status: "No infos found!" });
