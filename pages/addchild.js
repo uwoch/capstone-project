81f39f32104888df37f4d4b2874b81cfb3f65151
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-09-08 06:54:15 +0200

    final touch to adding a child

--- pages/addchild.js
+++ pages/addchild.js
@@ -1,28 +1,47 @@
 import KidForm from "@/components/KidForm";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function AddChildPage() {
   const router = useRouter();
-  
+  const [loading, setLoading] = useState(false);
+
   async function addChild(event) {
     event.preventDefault();
+    setLoading(true);
+
     const formData = new FormData(event.target);
     const kidData = Object.fromEntries(formData);
-  
-    const res = await fetch("api/kids", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(kidData),
-    });
-    if (res.status === 400) 
-    if (res.status === 418) 
-    if (res.ok) {
-      router.push("/");
+
+    try {
+      const res = await fetch("api/kids", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(kidData),
+      });
+
+      if (res.status === 400) {
+        console.error("Bad Request");
+      } else if (res.status === 418) {
+        console.error("Es ist ein unerwarteter Fehler aufgetreten. Bitte versuchen Sie es erneut.");
+      } else if (res.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error("Ein Fehler ist aufgetreten:", error);
+    } finally {
+      setLoading(false);
     }
   }
+
   return (
     <>
-      <KidForm onSubmit={addChild} />
+      {loading ? (
+        <p>Please wait...</p>
+      ) : (
+        <KidForm onSubmit={addChild} />
+      )}
     </>
   );
 }
+
