0912242db8edf91d3d978ecb2ab661022f67be35
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-09-15 13:19:41 +0200

    and another try

--- components/KidForm/index.js
+++ components/KidForm/index.js
@@ -75,7 +75,6 @@ export default function KidForm({ isEditMode, name, birthDate }) {
           alt={imageId ? "Bildvorschau" : "Platzhalterbild"}
           width="250"
           height="250"
-          imageId={kidData.imageId}
         />
       <StyledSaveButton type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#016e82" viewBox="0 0 16 16">
           <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
