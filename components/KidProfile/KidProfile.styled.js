d4caaf985f04e7eec2a00971f0504581c6bdae47
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-09-22 09:04:59 +0200

    final change

--- components/KidProfile/KidProfile.styled.js
+++ components/KidProfile/KidProfile.styled.js
@@ -17,40 +17,39 @@ export const StyledEventList = styled.ul`
   align-items: center;
 `;
 
-export const StyledName = styled.h2`    
+export const StyledName = styled.h2`
   text-align: center;
 `;
 
-export const StyledBirthDate = styled.h3`    
+export const StyledBirthDate = styled.h3`
   text-align: center;
   margin-top: -1rem;
 `;
 
 export const StyledImage = styled(Image)`
-    width: 80%;
+  width: 80%;
+  height: auto;
+  border-radius: 0.5rem;
+  margin-bottom: -0.8rem;
+
+  @media (min-width: 600px) {
+    width: 60%;
     height: auto;
-    border-radius: 0.5rem;
-    margin-bottom: -0.8rem;
+  }
 
-    @media (min-width: 600px){
-        width: 60%;
-        height: auto;
-    }
+  @media (min-width: 900px) {
+    width: 40%;
+    height: auto;
+  }
 
-    @media (min-width: 900px){
-        width: 40%;
-        height: auto;
-    }
-
-    @media (min-width: 1200px){
-        width: 40%;
-        height: auto;
-    }
-    @media (min-width: 1536px){
-        width: 20%;
-        height: auto;
-    }
-
+  @media (min-width: 1200px) {
+    width: 40%;
+    height: auto;
+  }
+  @media (min-width: 1536px) {
+    width: 20%;
+    height: auto;
+  }
 `;
 export const StyledKidDetails = styled.section`
   display: flex;
@@ -58,11 +57,11 @@ export const StyledKidDetails = styled.section`
   align-items: center;
 `;
 
-export const StyledHeading = styled.h3`
-`;
+export const StyledHeading = styled.h3``;
+
 export const StyledKidCard = styled.section`
-box-shadow: 0px 1px 5px -2px #7f8487;
+  box-shadow: 0px 1px 5px -2px #7f8487;
   border-radius: 0.5rem;
-  padding-bottom: 0.5rem;
+  padding: 0.5rem;
   margin-bottom: 3rem;
 `;
